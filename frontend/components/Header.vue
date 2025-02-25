c79ef9aea988a1420792f1638a668f76306e22c8
Evgeniy Golovkin <evgeniq.golovkin@gmail.com>
2025-02-26 03:23:27 +0500

    feat: redesign header & small fixes

--- frontend/components/Header.vue
+++ frontend/components/Header.vue
@@ -1,12 +1,20 @@
 <script setup lang="ts">
+import { useDisplay } from "vuetify";
+
 interface IMenu {
   title: string
   link: string
 }
 
+const { smAndUp } = useDisplay()
+
 const drawer = ref<boolean>(false)
 
 const menu: IMenu[] = [
+  {
+    title: 'Главная',
+    link: '/'
+  },
   {
     title: 'Новости',
     link: '/news',
@@ -35,85 +43,91 @@ const menu: IMenu[] = [
 </script>
 
 <template>
-  <v-app-bar class="header d-flex flex-column position-relative bg-transparent" elevation="0" :height="150"  >
-    <v-container class="bg-transparent mx-0" style="max-width: 100%;">
-      <div class="header-top d-flex justify-space-between">
-        <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer" class="d-block d-sm-none" height="24" width="24"></v-app-bar-nav-icon>
-        <nav class="d-none d-sm-block">
+  <v-app-bar class="header d-flex flex-column position-relative bg-transparent" elevation="0" :height="smAndUp ? 150 : 68"  >
+    <v-container
+        class="bg-transparent mx-0"
+        :style="[
+            {backgroundColor: !smAndUp ? '#141515 !important' : ''},
+        ]"
+        style="max-width: 100%;"
+    >
+      <div class="header-top d-flex align-center justify-space-between">
+        <v-app-bar-nav-icon
+            variant="text"
+            @click.stop="drawer = !drawer"
+            class="d-block d-md-none"
+            height="24"
+            width="24"
+        />
+        <nav class="d-none d-md-block">
           <ul class="d-flex ga-4">
-            <li v-for="item in menu" :key="item.title">
-              <nuxt-link :to="item.link" class="hover" exact-active-class="text-primary">
+            <li
+                v-for="(item, index) in menu"
+                :key="item.title"
+                v-show="index !== 0"
+            >
+              <nuxt-link
+                  :to="item.link"
+                  class="hover text-body-2 text-lg-body-1"
+                  exact-active-class="text-primary"
+              >
                 {{ item.title }}
               </nuxt-link>
             </li>
           </ul>
         </nav>
-        <div class="d-flex justify-space-between ga-8">
-          <div class="d-flex align-center ga-2 hover">
-            <v-icon icon="mdi-phone" size="18"></v-icon>
-            <a href="tel:+7 777 121 2342">+7 (777) 121-23-42</a>
+        <div class="d-none d-sm-flex align-center justify-space-between ga-2">
+          <div class="d-flex align-center ga-2">
+            <p class="text-primary text-caption">Алматы</p>
           </div>
+          |
           <div class="d-flex align-center ga-2 hover">
-            <v-icon icon="mdi-email" size="18"></v-icon>
-            <a href="mailto:egorovasofia632@gmail.com">egorovasofia632@gmail.com</a>
+            <a href="tel:+7 777 121 2342" class="text-caption">+7 (777) 121-23-42</a>
           </div>
+          |
+          <div class="d-flex align-center ga-2 hover">
+            <a href="mailto:egorovasofia632@gmail.com" class="text-caption">egorovasofia632@gmail.com</a>
+          </div>
+        </div>
+        <div class="d-flex d-sm-none align-center justify-end ga-4 ga-md-10">
+          <nuxt-link to="/favorites" class="header-favorites__link">
+            <v-icon icon="mdi-heart" color="primary" size="35"></v-icon>
+            <label class="header-favorites__link-label text-caption">0</label>
+          </nuxt-link>
+          <v-btn to="/cart"  variant="flat" color="secondary">
+            <div class="d-flex align-center ga-2">
+              <v-icon icon="mdi-cart" size="28"></v-icon>
+              <p>0 ₸</p>
+            </div>
+          </v-btn>
         </div>
       </div>
     </v-container>
-    <v-container class="text-white mx-0 mx-md-auto" style="max-width: 100%; background-color: #141515; z-index: 1004">
+    <v-container
+        class="d-none d-sm-block text-white mx-0 mx-md-auto"
+        style="max-width: 100%; background-color: #141515; z-index: 1004"
+    >
       <v-row>
-        <v-col cols="10" sm="6" md="8" class="d-flex flex-column-reverse flex-sm-column flex-md-row align-end align-md-center justify-end ga-2 ga-md-10">
-          <div class="d-none d-sm-block">
-            <v-menu open-on-hover offset-y>
-              <template v-slot:activator="{ props }">
-                <v-btn v-bind="props" class="d-flex align-center">
-                  <v-icon icon="mdi-cash-multiple" color="secondary" class="mb-1 mr-2" size="30"></v-icon>
-                  <p class="text-h6">Оплата</p>
-                </v-btn>
-              </template>
-
-              <v-list class="px-4 py-2">
-                <p class="text-h5 mb-3">Оплата</p>
-                <ul class="d-flex flex-column ga-2 mb-2 list-point">
-                  <li>Оплата наличными в офисе</li>
-                  <li>Оплата наличными курьеру</li>
-                  <li>Оплата платежной картой при получении</li>
-                  <li>Оплата платежной картой на сайте</li>
-                  <li>Безналичный расчет через бан</li>
-                  <li>Рассрочка</li>
-                  <li>Кредит</li>
-                </ul>
-              </v-list>
-            </v-menu>
-          </div>
-          <div class="d-none d-sm-block">
-            <v-menu open-on-hover offset-y>
-              <template v-slot:activator="{ props }">
-                <v-btn v-bind="props" class="d-flex align-center">
-                  <v-icon icon="mdi-truck-fast" color="secondary" class="mr-3" size="30"></v-icon>
-                  <p class="text-h6">Доставка</p>
-                </v-btn>
-              </template>
-
-              <v-list class="px-4 py-2">
-                <p class="text-h5 mb-3">Доставка</p>
-                <ul class="d-flex flex-column ga-2 mb-2 list-point">
-                  <li>Курьерская доставка по городу</li>
-                  <li>Самовывоз из нашего магазина</li>
-                  <li>Доставка в регионы Казахстана</li>
-                </ul>
-              </v-list>
-            </v-menu>
-          </div>
-          <div class="px-4">
-            <nuxt-link to="/cart" class="d-flex align-center ga-2">
-              <v-icon icon="mdi-cart" size="40" color="secondary"></v-icon>
-              <div class="d-flex flex-column">
-                <p>0 шт.</p>
-                <p>0 ₸</p>
-              </div>
-            </nuxt-link>
-          </div>
+        <v-col cols="12" sm="8" class="d-none d-sm-flex align-center ga-6">
+          <nuxt-link to="/" class="text-h5 text-md-h4 text-primary">SHOP AUDIO</nuxt-link>
+          <v-btn variant="flat" color="secondary">
+            <div class="d-flex align-center ga-2">
+              <v-icon icon="mdi-format-list-bulleted"></v-icon>
+              <p>КАТАЛОГ</p>
+            </div>
+          </v-btn>
+        </v-col>
+        <v-col cols="12" sm="4" class="d-flex align-center justify-end ga-4 ga-md-10">
+          <nuxt-link to="/favorites" class="header-favorites__link">
+            <v-icon icon="mdi-heart" color="primary" size="35"></v-icon>
+            <label class="header-favorites__link-label text-caption">0</label>
+          </nuxt-link>
+          <v-btn to="/cart"  variant="flat" color="secondary">
+            <div class="d-flex align-center ga-2">
+              <v-icon icon="mdi-cart" size="28"></v-icon>
+              <p>0 ₸</p>
+            </div>
+          </v-btn>
         </v-col>
       </v-row>
     </v-container>
@@ -121,61 +135,47 @@ const menu: IMenu[] = [
   <v-navigation-drawer
       v-model="drawer"
       temporary
-      :style="[drawer ? 'width: 100%; top: 56px; zIndex: 1006; height: calc(100% - 56px); display: block' : '']"
+      :style="[drawer ? 'width: 100%; top: 68px; zIndex: 1006; height: calc(100% - 68px); display: block' : '']"
+      class="bg-background-card"
   >
     <div class="mb-4">
+      <p class="text-h5 text-md-h4 text-primary mt-4 ml-4">SHOP AUDIO</p>
+      <v-divider class="my-3"></v-divider>
+      <v-row class="mx-1">
+        <v-col cols="12" class="d-flex d-sm-none align-center ga-6">
+          <div class="d-flex flex-column ga-4">
+            <div class="d-flex align-center ga-2">
+              <v-icon icon="mdi-map-marker-radius" color="primary"></v-icon>
+              <p class="text-primary text-body-2">Алматы</p>
+            </div>
+            <div class="d-flex align-center ga-2 hover">
+              <v-icon icon="mdi-phone" color="primary"></v-icon>
+              <a href="tel:+7 777 121 2342" class="text-body-2">+7 (777) 121-23-42</a>
+            </div>
+            <div class="d-flex align-center ga-2 hover">
+              <v-icon icon="mdi-email" color="primary"></v-icon>
+              <a href="mailto:egorovasofia632@gmail.com" class="text-body-2">egorovasofia632@gmail.com</a>
+            </div>
+          </div>
+        </v-col>
+      </v-row>
+      <v-divider class="mt-3 mb-2"></v-divider>
       <ul class="d-flex flex-column ga-4">
-        <li v-for="(item, index) in menu" :key="item.title" :class="[index === 0 ? 'mt-3' : '']" :style="[index !== 0 ? 'marginTop: -4px' : '']">
-          <nuxt-link :to="item.link" class="hover text-h5 mx-4" exact-active-class="text-secondary">
+        <li
+            v-for="(item, index) in menu"
+            :key="item.title"
+            :style="[index !== 0 ? 'marginTop: -4px' : '']"
+        >
+          <nuxt-link
+              :to="item.link"
+              class="hover text-h5 mx-4"
+              exact-active-class="text-primary"
+          >
             {{ item.title }}
           </nuxt-link>
           <v-divider class="mt-2"></v-divider>
         </li>
       </ul>
-      <div class="d-flex justify-space-between flex-wrap mt-4 ga-2">
-        <div>
-          <v-menu offset-y>
-            <template v-slot:activator="{ props }">
-              <v-btn v-bind="props" class="d-flex align-center" elevation="0">
-                <v-icon icon="mdi-cash-multiple" color="secondary" class="mb-1 mr-2" size="30"></v-icon>
-                <p class="text-h6">Оплата</p>
-              </v-btn>
-            </template>
-
-            <v-list class="px-4 py-2">
-              <p class="text-h5 mb-3">Оплата</p>
-              <ul class="d-flex flex-column ga-2 mb-2 list-point">
-                <li>Оплата наличными в офисе</li>
-                <li>Оплата наличными курьеру</li>
-                <li>Оплата платежной картой при получении</li>
-                <li>Оплата платежной картой на сайте</li>
-                <li>Безналичный расчет через бан</li>
-                <li>Рассрочка</li>
-                <li>Кредит</li>
-              </ul>
-            </v-list>
-          </v-menu>
-        </div>
-        <div>
-          <v-menu offset-y>
-            <template v-slot:activator="{ props }">
-              <v-btn v-bind="props" class="d-flex align-center" elevation="0">
-                <v-icon icon="mdi-truck-fast" color="secondary" class="mr-3" size="30"></v-icon>
-                <p class="text-h6">Доставка</p>
-              </v-btn>
-            </template>
-
-            <v-list class="px-4 py-2">
-              <p class="text-h5 mb-3">Доставка</p>
-              <ul class="d-flex flex-column ga-2 mb-2 list-point">
-                <li>Курьерская доставка по городу</li>
-                <li>Самовывоз из нашего магазина</li>
-                <li>Доставка в регионы Казахстана</li>
-              </ul>
-            </v-list>
-          </v-menu>
-        </div>
-      </div>
     </div>
   </v-navigation-drawer>
 </template>
@@ -185,6 +185,32 @@ const menu: IMenu[] = [
   .v-toolbar__content {
     display: flex;
     flex-direction: column;
+  }
+  &-favorites__link {
+    position: relative;
+    &:after {
+      content: '';
+      position: absolute;
+      bottom: 0;
+      right: 0;
+      background: black;
+      width: 22px;
+      height: 22px;
+      border-radius: 50px;
+      padding: 0 8px;
+      z-index: 1;
+      pointer-events: none;
+    }
+    &-label {
+      pointer-events: none;
+      position: absolute;
+      bottom: .2px;
+      right: .7px;
+      background: rgb(var(--v-theme-secondary));
+      border-radius: 50px;
+      padding: 0 6px;
+      z-index: 2;
+    }
   }
 }
 </style>
--- frontend/components/Main/News.vue
+++ frontend/components/Main/News.vue
@@ -5,12 +5,29 @@ const store = useStore()
 <template>
   <div class="bg-background-card pa-4 rounded-lg">
     <v-row class="main-news-sheet-row">
-      <v-col cols="12" sm="6" v-for="card in store.GET_NEWS_MAIN" :key="card.id">
-        <v-card :to="`/news/${card.slug}`" elevation="16" rounded="lg" class="d-flex flex-column h-100 bg-background-card">
-          <v-img :src="card.image" width="100%" height="100%" max-height="220px" cover />
-          <div class="pa-4 d-flex flex-column flex-grow-1">
+      <v-col
+          cols="12"
+          sm="6"
+          v-for="card in store.GET_NEWS_MAIN"
+          :key="card.id"
+      >
+        <v-card
+            :to="`/news/${card.slug}`"
+            elevation="16"
+            rounded="lg"
+            height="100%"
+            class="d-flex flex-column bg-background-card"
+        >
+          <v-img
+              :src="card.image"
+              width="100%"
+              height="220px"
+              max-height="220px"
+              cover
+          />
+          <div class="pa-4 d-flex flex-column">
             <p class="mb-4 text-primary">{{ formatDate(card.date, 'dots') }}</p>
-            <p class="flex-grow-1">{{ card.short_description }}</p>
+            <p>{{ card.short_description }}</p>
           </div>
         </v-card>
       </v-col>
