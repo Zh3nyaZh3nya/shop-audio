49b17769244dcf8d14ebace2d0e049e5f3d5017b
Evgeniy Golovkin <evgeniq.golovkin@gmail.com>
2025-02-27 03:22:16 +0500

    feat: add more static productsData

---
+++ frontend/pages/catalog/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const store = useStore()
+</script>
+
+<template>
+  <v-main>
+    <v-container>
+
+    </v-container>
+  </v-main>
+</template>
+
+<style scoped>
+
+</style>
